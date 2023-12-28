7964e2d07f84df7a278065a23aaec32c9bf40683
Dina Migunova <migunova.dina@gmail.com>
2023-12-28 22:09:01 +0100

    Add react-template without CRA

---
+++ src/module.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import img from './img/img1.jpg'
+
+const Module = () => (
+    <>
+        <h1>Hello, world</h1>
+        <img src={img}/>
+    </>
+);
+
+export default Module;
